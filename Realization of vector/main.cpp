5650a06f3772087424f2ac8a6f8cd11722a921db
RZeroF1 <RZeroF1@outlook.com>
2025-01-17 18:07:44 +0800

    Modified

--- Realization of vector/main.cpp
+++ Realization of vector/main.cpp
@@ -4,21 +4,44 @@ using namespace std;
 
 int main()
 {
-	I::vector<int> tmp;
-	tmp.pushback(1);
-	tmp.pushback(2);
-	tmp.pushback(3);
-	tmp.pushback(4);
-	//tmp.pushback(5);
+	myContainer::vector<int> tmp;
+	tmp.push_back(1);
+	tmp.push_back(2);
+	tmp.push_back(3);
+	tmp.push_back(4);
+	//tmp.push_back(5);
 
-	display_int(tmp);
+	display_vector(tmp);
 
 	tmp.insert(tmp.begin() + 2, 30);
-	display_int(tmp);
+	display_vector(tmp);
 
 	auto pos = find(tmp.begin(), tmp.end(), 4);
 	tmp.insert(pos, 200);
-	display_int(tmp);
+	//After insertion, the value in the position will not be origin(iterator fails)
+	display_vector(tmp);
+
+	//remove all the odd numbers(iterator fails)
+	//auto it = tmp.begin();
+	//while (it != tmp.end())
+	//{
+	//	if (*it % 2 == 0)
+	//	{
+	//		tmp.erase(it);
+	//		display_vector(tmp);
+	//	}
+	//	++it;
+	//}
+
+	pos = find(tmp.begin(), tmp.end(), 4);
+	tmp.erase(pos);
+	display_Container(tmp);
+
+	int i = int();
+	int j = int(1);
+	double k(2);
+
+	myContainer::vector<int> ins(tmp);
 
 	return 0;
 }
--- Realization of vector/vector.h
+++ Realization of vector/vector.h
@@ -3,17 +3,36 @@
 #include <memory>
 #include <stdexcept>
 
-namespace I
+namespace myContainer
 {
-	template<class T>
+	template<class valueType>
 	class vector
 	{
 	public:
-		typedef T* iterator;
-		typedef const T* const_iterator;
+		typedef valueType* iterator;
+		typedef const valueType* const_iterator;
 
 		vector()
 		{
+			_start = _finish = _end_storage = nullptr;
+		}
+
+		vector(const vector<valueType>& instance)
+		{
+			reserve(instance.capacity());
+			for (auto& e : instance)
+			{
+				push_back(e);
+			}
+		}
+
+		~vector()
+		{
+			if (_start)
+			{
+				delete[] _start;
+			}
+
 			_start = _finish = _end_storage = nullptr;
 		}
 
@@ -22,14 +41,14 @@ namespace I
 			return _start;
 		}
 
-		iterator end()
-		{
-			return _finish;
-		}
-
 		const_iterator begin() const
 		{
 			return _start;
+		}
+
+		iterator end()
+		{
+			return _finish;
 		}
 
 		const_iterator end() const
@@ -42,7 +61,17 @@ namespace I
 			return _end_storage - _start;
 		}
 
+		const size_t capacity() const
+		{
+			return _end_storage - _start;
+		}
+
 		size_t size()
+		{
+			return _finish - _start;
+		}
+
+		const size_t size() const
 		{
 			return _finish - _start;
 		}
@@ -57,8 +86,8 @@ namespace I
 			if (allocateSpace > capacity())
 			{
 				size_t old_size = size();
-				iterator new_start = new T[allocateSpace];
-				memcpy(new_start, _start, size() * sizeof(T));
+				iterator new_start = new valueType[allocateSpace];
+				memcpy(new_start, _start, size() * sizeof(valueType));
 				delete[] _start;
 
 				_start = new_start;
@@ -67,7 +96,23 @@ namespace I
 			}
 		}
 
-		void pushback(const T& instance)
+		void resize(size_t n, valueType value = valueType())
+		{
+			if (n < size())
+			{
+				_finish = _start + n;
+			}
+			else
+			{
+				reserve(n);
+				while (_finish < _start + n)
+				{
+					*_finish++ = value;
+				}
+			}
+		}
+
+		void push_back(const valueType& instance)
 		{
 			if (_finish == _end_storage)
 			{
@@ -77,7 +122,7 @@ namespace I
 			*_finish++ = instance;
 		}
 
-		void popback()
+		void pop_back()
 		{
 			if (empty())
 			{
@@ -87,7 +132,7 @@ namespace I
 			--_finish;
 		}
 
-		void insert(iterator pos, const T& instance)
+		void insert(iterator pos, const valueType& instance)
 		{
 			if (_finish == _end_storage)
 			{
@@ -110,7 +155,54 @@ namespace I
 			++_finish;
 		}
 
-		T& operator[](size_t num)
+		void erase(iterator pos)
+		{
+			if ((pos < _start) || (pos >= _finish))
+			{
+				throw std::out_of_range("Index out of range");
+			}
+
+			iterator i = pos;
+			while (i < _finish - 1)
+			{
+				*i = *(i + 1);
+				++i;
+			}
+
+			--_finish;
+		}
+
+		void clear()
+		{
+			_finish = _start;
+		}
+
+		vector<valueType>& operator=(const vector<valueType>& instance)
+		{
+			if (this != &instance)
+			{
+				clear();
+
+				reserve(instance.capacity());
+				for (auto e : instance)
+				{
+					push_back(e);
+				}
+
+				return *this;
+			}
+		}
+
+		vector<valueType>& operator=(vector<valueType> instance)
+		{
+			std::swap(_start, instance._start);
+			std::swap(_finish, instance._finish);
+			std::swap(_end_storage, instance._end_storage);
+
+			return *this;
+		}
+
+		valueType& operator[](size_t num)
 		{
 			if (num >= size())
 				throw std::out_of_range("Index out of range");
@@ -125,12 +217,35 @@ namespace I
 	};
 
 	template<class T>
-	void display_int(const vector<T>& instance)
+	void display_vector(const vector<T>& instance)
 	{
 		typename vector<T>::const_iterator it = instance.begin();
-		//The compiler does not know const_iterator in "vector<T>"
+		//The compiler does not know const_iterator in "vector<valueType>"
 		//use typename to declare vector<int> is a type
 		//or auto to determine automatically
+		auto it_ = instance.begin();
+
+		while (it != instance.end())
+		{
+			std::cout << *it << " ";
+			++it;
+		}
+
+		std::cout << std::endl;
+
+		for (auto e : instance)
+		{
+			std::cout << e << " ";
+		}
+
+		std::cout << std::endl;
+
+	}
+
+	template<class Container>
+	void display_Container(const Container& instance)
+	{
+		typename Container::const_iterator it = instance.begin();
 		auto it_ = instance.begin();
 
 		while (it != instance.end())
